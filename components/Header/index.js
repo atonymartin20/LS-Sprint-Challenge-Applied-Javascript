09522b68f29085e710d94a8ac2840c43a484bed4
Alex Martin <atonymartin20@yahoo.com>
2019-08-14 12:10:41 -0500

    Header/index.js completed

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,6 +7,29 @@
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
-// And add it to the DOM in the .headerContainer component
+// And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header());
+
+function Header() {
+    const headerDiv = document.createElement('div');
+    headerDiv.classList.add('header');
+
+    const headerDateSpan = document.createElement('span');
+    headerDateSpan.classList.add('date');
+    headerDateSpan.textContent = 'MARCH 28, 2019';
+
+    const headerH1 = document.createElement('h1');
+    headerH1.textContent = 'Lambda Times';
+
+    const headerTempSpan = document.createElement('span');
+    headerTempSpan.classList.add('temp');
+    headerTempSpan.textContent = '98°';
+
+    headerDiv.appendChild(headerDateSpan);
+    headerDiv.appendChild(headerH1);
+    headerDiv.appendChild(headerTempSpan);
+
+    return headerDiv
+}
